955756d818f8bf627ed839eaa3687d721c8d5310
gian402 <pierovilca656@gmail.com>
2026-05-07 14:16:28 -0500

    config: add gh-pages deploy setup

--- horus-react/vite.config.js
+++ horus-react/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Horus/',
   plugins: [react()],
   server: {
     allowedHosts: ['goatskin-anime-self.ngrok-free.dev', 'all'],
